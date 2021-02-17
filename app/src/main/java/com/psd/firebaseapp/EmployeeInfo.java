2df9d96ebe02eedb6fcb98854139a8d9901dc966
Er. prem singh daksha <46933759+premsingh8171@users.noreply.github.com>
2021-02-17 17:15:30 +0530

    phone authentication

---
+++ app/src/main/java/com/psd/firebaseapp/EmployeeInfo.java
@@ -0,0 +1,40 @@
+package com.psd.firebaseapp;
+
+public class EmployeeInfo {
+    //string variable for storing employee name.
+    private String employeeName;
+    //string variable for stoing employee contact number
+    private String employeeContactNumber;
+    //string variable for storing employee address.
+    private String employeeAddress;
+
+    //an empty constructor is required when using Firebase Realtie Database.
+    public EmployeeInfo(){
+
+    }
+
+    //created getter and setter methods for all our variables.
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeContactNumber() {
+        return employeeContactNumber;
+    }
+
+    public void setEmployeeContactNumber(String employeeContactNumber) {
+        this.employeeContactNumber = employeeContactNumber;
+    }
+
+    public String getEmployeeAddress() {
+        return employeeAddress;
+    }
+
+    public void setEmployeeAddress(String employeeAddress) {
+        this.employeeAddress = employeeAddress;
+    }
+}
